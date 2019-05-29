be1ed26ae625ca37b3e9d4c04eaed8393230a065
Reyoth <serialkiwi@gmail.com>
2019-05-29 12:18:19 +0200

    ajout jeux2 et modif variables

--- 00_algorithmique/01_python/boucles.py
+++ 00_algorithmique/01_python/boucles.py
@@ -2,11 +2,11 @@
 
 #While > tant que...
 # avec un compteur qui diminue a chaque tour
-#compteur = 10
 
-#while compteur >= 0:
-#    print("le cours est fini dans :", compteur)
-#    compteur = compteur - 1
+compteur = 10
+while compteur >= 0:
+    print("le cours est fini dans :", compteur)
+    compteur = compteur - 1
 
 # avec un boolean
 flag = True
