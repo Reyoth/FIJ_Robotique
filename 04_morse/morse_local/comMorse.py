3f7fe2d4f822c2ae56ac075687270571b267bbb0
Reyoth <serialkiwi@gmail.com>
2019-06-11 15:02:19 +0200

    ajout morse reseau

--- 04_morse/morse_local/comMorse.py
+++ 04_morse/morse_local/comMorse.py
@@ -97,3 +97,36 @@ print(reponse)
 print("tape un code")
 code = input()
 print( decode(code) ) """
+
+
+def encodeTexte(phrase):
+    """Fonction prennant une phrase ou un mot et renvoyant son equivalent en morse
+    sous forme de chaine de caractere.
+    Pour faciliter la lecture, des espaces sont inserer entre les codes.
+    Des / sont aussi inserer pour representer les espaces entre les mots"""
+    
+    reponse=""
+    for lettre in phrase:
+        if lettre != " " and lettre != "":
+            reponse= reponse + encode(lettre)
+            reponse= reponse + " "
+        else:
+            reponse= reponse + "/ "
+    return reponse
+
+
+
+def decodeMorse(morseBrut):
+    """Fonction prennant une phrase ou un mot  en morse et renvoyant son equivalent texte
+    sous forme de chaine de caractere."""
+    
+    reponse=""
+    morseBrut = str.split(morseBrut," ")
+    for element in morseBrut:
+            if element != "/" and element != "":
+                reponse= reponse + decode(element)
+            elif element == "/":
+                reponse= reponse + " "
+            else:
+                pass
+    return reponse
